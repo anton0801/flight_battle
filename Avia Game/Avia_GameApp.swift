dc9548d4615db15fffc29aa80984e57368b094f8
Anton <danilovanton724@gmail.com>
2024-04-07 14:20:27 +0200

    Initial Commit

---
+++ Avia Game/Avia_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Avia_GameApp.swift
+//  Avia Game
+//
+//  Created by Anton on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Avia_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Avia Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Avia Game
+//
+//  Created by Anton on 7/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
